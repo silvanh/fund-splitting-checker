4183d2d71190949ec2e0185961c5f88dfdec9dbb
Silvan Habegger <silvan.habegger@gmail.com>
2024-10-10 17:32:54 +0200

    add tests and implement method of PercentageChecker

--- fund-splitting-checker/src/main/java/com/silvanh/PercentageChecker.java
+++ fund-splitting-checker/src/main/java/com/silvanh/PercentageChecker.java
@@ -17,7 +17,7 @@ public final class PercentageChecker {
      * 
      * @param percentages A list of positive {@code Double} values representing
      *                    fractions (should be in the range [0.0, 1.0]).
-     * @param TOLERANCE   A {@code Double} value representing the tolerance level
+     * @param TOLERANCE   A {@code double} value representing the tolerance level
      *                    for comparison to 1. The method will consider the sum to
      *                    be equal to 1 if the absolute difference is less than or
      *                    equal to this tolerance.
@@ -28,7 +28,22 @@ public final class PercentageChecker {
      * @throws IllegalArgumentException if any percentage in the list is negative or
      *                                  if the tolerance is negative.
      */
-    public static boolean checkIfPercentagesEquals100(List<Double> percentages, Double TOLERANCE) {
-        return true;
+    public static boolean checkIfPercentagesEquals100(List<Double> percentages, double TOLERANCE) {
+        if (percentages == null || percentages.isEmpty()) {
+            throw new IllegalArgumentException("Percentages must have at least one entry");
+        }
+
+        for (Double percentage : percentages) {
+            if (percentage < 0) {
+                throw new IllegalArgumentException("Percentages must be non-negative.");
+            }
+        }
+    
+        if (TOLERANCE < 0) {
+            throw new IllegalArgumentException("Tolerance must be non-negative.");
+        }
+    
+        double total = percentages.stream().mapToDouble(Double::doubleValue).sum();
+        return Math.abs(total - 1.0) <= TOLERANCE;
     }
 }
--- fund-splitting-checker/src/test/java/com/silvanh/PercentageCheckerTest.java
+++ fund-splitting-checker/src/test/java/com/silvanh/PercentageCheckerTest.java
@@ -3,7 +3,6 @@ package com.silvanh;
 import static org.junit.Assert.*;
 
 import java.util.Arrays;
-import java.util.List;
 
 import org.junit.Test;
 
@@ -11,8 +10,71 @@ public class PercentageCheckerTest {
 
     @Test
     public void ifPercentagesSumUpTo1AndToleranceZeroThenExpectTrue() {
-        List<Double> percentages = Arrays.asList(0.5, 0.5);
-        boolean result = PercentageChecker.checkIfPercentagesEquals100(percentages, 0d);
+        boolean result = PercentageChecker
+            .checkIfPercentagesEquals100(Arrays.asList(0.5, 0.5), 0.0);
         assertTrue(result); 
+    }
+
+    @Test
+    public void ifPercentagesSumUpTo1ButHaveOnlyOneElementAndToleranceZeroThenExpectTrue() {
+        boolean result = PercentageChecker
+            .checkIfPercentagesEquals100(Arrays.asList(1.0), 0.0);
+        assertTrue(result); 
+    }
+
+    @Test
+    public void ifPercentagesSumUpWithinToleranceAndToleranceIsNotZeroThenExpectTrue() {
+        boolean result = PercentageChecker
+            .checkIfPercentagesEquals100(Arrays.asList(0.1, 0.4, 0.1, 0.35), 0.1);
+        assertTrue(result); 
+    }
+
+    @Test
+    public void ifPercentagesSumUpWithinExactlyToToleranceAndToleranceIsNotZeroThenExpectTrue() {
+        boolean result = PercentageChecker
+            .checkIfPercentagesEquals100(Arrays.asList(0.1, 0.4, 0.1, 0.3), 0.1);
+        assertTrue(result); 
+    }
+
+    @Test
+    public void ifPercentagesSumUpNotTo1AndToleranceZeroThenExpectFalse() {
+        boolean result = PercentageChecker
+            .checkIfPercentagesEquals100(Arrays.asList(0.5, 0.4), 0.0);
+        assertFalse(result); 
+    }
+
+    @Test
+    public void ifPercentagesAreNullButToleranceIsOkExpectIllegalArgumentException() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            PercentageChecker.checkIfPercentagesEquals100(null, 0.0);
+        });
+    }
+
+    @Test
+    public void ifPercentagesHaveNegativeNumberButToleranceIsOkExpectIllegalArgumentException() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            PercentageChecker.checkIfPercentagesEquals100(Arrays.asList(0.5, -0.4), 0.0);
+        });
+    }
+
+    @Test
+    public void ifPercentagesHaveAllNegativeNumberButToleranceIsOkExpectIllegalArgumentException() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            PercentageChecker.checkIfPercentagesEquals100(Arrays.asList(-0.5, -0.4), 0.0);
+        });
+    }
+
+    @Test
+    public void ifPercentagesIsOnlyOneNegativeNumberButToleranceIsOkExpectIllegalArgumentException() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            PercentageChecker.checkIfPercentagesEquals100(Arrays.asList(-0.5), 0.0);
+        });
+    }
+
+    @Test
+    public void ifPercentagesAreCorrectButToleranceIsNegativeNumberExpectIllegalArgumentException() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            PercentageChecker.checkIfPercentagesEquals100(Arrays.asList(0.5, 0.4, 0.1), -0.1d);
+        });
     }
 }
